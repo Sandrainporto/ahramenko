d670fd261e06275aa5188062ccf5f9295a5b164d
Alexandra Barmin <barmin.alexandra@gmail.com>
2023-11-25 00:13:45 +0300

    feat: change header and adapt it

--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -1,54 +1,20 @@
-import { NavLink } from 'react-router-dom';
+import Navigation from '../../entities/Navigation/Navigation';
 import styles from './Header.module.scss';
-import logo from './logo.png';
-
-interface Navigation {
-  path: string;
-  title: string;
-}
+import { useState } from 'react';
 
 const Header = () => {
-  const LeftNavigation: Navigation[] = [
-    { path: '/', title: 'Главная' },
-    { path: '/portfolio', title: 'Портфолио' },
-    { path: '/services', title: 'Услуги' },
-  ];
-  const RightNavigation: Navigation[] = [
-    { path: '/about-me', title: 'Обо мне' },
-    { path: '/testimonials', title: 'Отзывы' },
-    { path: '/contacts', title: 'Контакты' },
-  ];
+  const [burgerState, setBurgerState] = useState(false);
 
   return (
     <header className={styles.header}>
       <div className={styles.container}>
-        <nav className={styles.navigation}>
-          <ul className={styles.navigation_list}>
-            {LeftNavigation.map((link) => (
-              <li key={link.title}>
-                <NavLink className={styles.navigation_link} to={link.path}>
-                  {link.title}
-                </NavLink>
-              </li>
-            ))}
-          </ul>
-        </nav>
-        <div className={styles.logo}>
-          <NavLink to="/" className={styles.link}>
-            <img src={logo} alt="website-logo" />
-          </NavLink>
+        <Navigation state={burgerState} setState={setBurgerState} />
+        <div
+          className={burgerState ? styles.burger_active : styles.burger}
+          onClick={() => setBurgerState(!burgerState)}
+        >
+          {burgerState ? '✖' : 'Меню'}
         </div>
-        <nav className={styles.navigation}>
-          <ul className={styles.navigation_list}>
-            {RightNavigation.map((link) => (
-              <li key={link.title}>
-                <NavLink className={styles.navigation_link} to={link.path}>
-                  {link.title}
-                </NavLink>
-              </li>
-            ))}
-          </ul>
-        </nav>
       </div>
     </header>
   );
