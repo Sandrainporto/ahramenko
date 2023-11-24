a3d40d7e8213e218da811393455ce9c032fdc57f
Alexandra Barmin <barmin.alexandra@gmail.com>
2023-11-24 14:39:10 +0300

    add: add tel to footer

--- src/widgets/Footer/Footer.tsx
+++ src/widgets/Footer/Footer.tsx
@@ -9,6 +9,7 @@ const Footer = () => {
         <div className={styles.button}>
           <img src={logo} alt="website-logo" />
         </div>
+        <a className={styles.tel} href="tel:+375336253140">+375 (33) 625-31-40</a>
         <Link to="/contacts" className={styles.contacts}>
           Контакты
         </Link>
