a5422622d15420ee0211bf666827b9cfd59d7673
Alexandra Barmin <barmin.alexandra@gmail.com>
2023-11-24 14:39:58 +0300

    style: format file

--- src/widgets/Footer/Footer.tsx
+++ src/widgets/Footer/Footer.tsx
@@ -9,7 +9,9 @@ const Footer = () => {
         <div className={styles.button}>
           <img src={logo} alt="website-logo" />
         </div>
-        <a className={styles.tel} href="tel:+375336253140">+375 (33) 625-31-40</a>
+        <a className={styles.tel} href="tel:+375336253140">
+          +375 (33) 625-31-40
+        </a>
         <Link to="/contacts" className={styles.contacts}>
           Контакты
         </Link>
