9f312f3c03b3c55dc064f4b5abfb6f402f5fecc6
Alexandra Barmin <barmin.alexandra@gmail.com>
2023-11-28 14:18:44 +0300

    feat: add socials as icons / text-links with fontawesome

--- src/entities/Socials/Icons.tsx
+++ src/entities/Socials/Icons.tsx
@@ -1,34 +1,62 @@
 import styles from './Icons.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons/faInstagram';
 
-const Icons = () => {
+type IconsProps = {
+  textType?: boolean;
+};
+
+const Icons = ({ textType }: IconsProps) => {
   return (
-    <div className={styles.icons}>
-      <a className={styles.icon} href="tel:+375336253140">
-        <img
+    <div className={textType ? styles.icons_text : styles.icons}>
+      <a
+        className={textType ? styles.icon_text : styles.icon}
+        href="tel:+375336253140"
+      >
+        <FontAwesomeIcon
+          icon={faPhone}
+          style={{ color: '#6e6e6e', fontSize: '23' }}
+        />
+        {/* <img
           width="30"
           height="30"
           src="https://img.icons8.com/glyph-neue/35/6e6e6e/phone.png"
           alt="phone"
-        />
+        /> */}
+        {textType ? 'Позвонить' : ''}
       </a>
       <a
-        className={styles.icon}
+        className={textType ? styles.icon_text : styles.icon}
         href="https://www.instagram.com/sandraahramenko/"
       >
-        <img
+        <FontAwesomeIcon
+          icon={faInstagram}
+          style={{ color: '#6e6e6e', fontSize: '28' }}
+        />
+        {/* <img
           width="30"
           height="30"
           src="https://img.icons8.com/ios/35/6e6e6e/instagram-new--v1.png"
           alt="instagram-new--v1"
-        />
+        /> */}
+        {textType ? 'Instagram' : ''}
       </a>
-      <a className={styles.icon} href="mailto:#&body=Запись на фотосессию">
-        <img
+      <a
+        className={textType ? styles.icon_text : styles.icon}
+        href="mailto:#&body=Запись на фотосессию"
+      >
+        <FontAwesomeIcon
+          icon={faEnvelope}
+          style={{ color: '#6e6e6e', fontSize: '28' }}
+        />
+        {/* <img
           width="30"
           height="30"
           src="https://img.icons8.com/parakeet-line/48/6e6e6e/new-post.png"
           alt="new-post"
-        />
+        /> */}
+        {textType ? 'Написать на почту' : ''}
       </a>
     </div>
   );
