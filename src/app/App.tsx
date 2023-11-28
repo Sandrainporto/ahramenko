3ecddba6f15f32c6bef82960241d22851d522bc5
Alexandra Barmin <barmin.alexandra@gmail.com>
2023-11-28 14:17:10 +0300

    feat: add wrapper

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,21 +2,25 @@ import Header from '../widgets/Header/Header';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Main from '../pages/Main/Main';
 import Error from '../pages/Error/Error';
-import Contacts from '../pages/Contacts.tsx';
+// import Contacts from '../pages/Contacts.tsx';
 import Footer from '../widgets/Footer/Footer.tsx';
+import Portfolio from '../pages/Portfolio/Portfolio.tsx';
+import './App.scss';
 
 const App = () => {
   return (
     <Router basename={import.meta.env.DEV ? '/' : '/ahramenko/'}>
-      <Header />
+      <div className="wrapper">
+        <Header />
 
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/contacts" element={<Contacts />} />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/family" element={<Portfolio />} />
 
-        <Route path="*" element={<Error />} />
-      </Routes>
-      <Footer />
+          <Route path="*" element={<Error />} />
+        </Routes>
+        <Footer />
+      </div>
     </Router>
   );
 };
--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,13 +1,12 @@
 import Photographer from '../../widgets/Photographer/Photographer';
 import Slider from '../../widgets/Slider/Slider';
 import Tiles from '../../widgets/TilesNavigation/Tiles';
-import styles from './_Main.module.scss';
 
 const Main = () => {
   return (
     <>
       <Slider />
-      <main className={styles.main}>
+      <main className="main">
         <Tiles />
         <Photographer />
       </main>
